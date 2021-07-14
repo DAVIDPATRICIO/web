8de8fd41d4546deb1e2a604cfeb47cba37f60eab
Mago Pato <46275040+DAVIDPATRICIO@users.noreply.github.com>
2021-07-14 14:59:33 -0500

    Add files via upload

--- ary.js
+++ ary.js
@@ -1,11 +1,12 @@
 presona ps = new presona("Ary Shardey",05,04,2003);
 String dia = "05/04";
 cumpleaños = ps.getCumpleaños();
-while(true){
-    if(dia.equals(cumpleaños)){
-        System.out.println(" happy birthday " + ps.getNombre());
-        break;
-    }else{
-    System.out.println(" hoy no es el cumpleaños de " + ps.getNombre());
+    while(true){
+        if(dia.equals(cumpleaños)){
+            System.out.println(" happy birthday " + ps.getNombre());
+            break;
+        }else{
+        System.out.println(" hoy no es el cumpleaños de " + ps.getNombre());
+        }
     }
 }
