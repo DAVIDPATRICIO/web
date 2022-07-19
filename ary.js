3a8ed6b12bd75c360ad635c0b86eb9ff50d0c7af
David Patricio Escamilla Marquez <46275040+MagoPato@users.noreply.github.com>
2022-07-19 03:01:54 -0500

    Update ary.js

--- ary.js
+++ ary.js
@@ -1,12 +1,12 @@
-presona ps = new presona ( "Ary Shardey",05,04,2003 ) ;
-String dia = "05/04";
-cumpleaños = ps.getCumpleaños();
+presona ps = new presona ( "Ary Shardey" , 05 , 04 , 2003 ) ;
+String dia = "05/04" ;
+cumpleaños = ps . getCumpleaños ( ) ;
     while ( true ) {
-        if( dia.equals ( cumpleaños ) ) {
-            System.out.println ( " happy birthday " + ps.getNombre() ) ;
+        if( dia . equals ( cumpleaños ) ) {
+            System . out . println ( " happy birthday " + ps . getNombre ( ) ) ;
             break ;
         } else {
-        System.out.println ( " hoy no es el cumpleaños de " + ps.getNombre() ) ;
+        System.out.println ( " hoy no es el cumpleaños de " + ps . getNombre ( ) ) ;
         }
     }
 }
